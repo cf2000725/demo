7a994e05b1eeb13261460acf90b117537a04209b
cf <1850146276@qq.com>
2019-06-09 16:53:34 +0800

    第二次提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("小三");
-        System.out.println("小四");
+        System.out.println("小四1");
     }
 
 }
